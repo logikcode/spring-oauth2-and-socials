bc693f64267153b23020f57269e06f2dd445b7e6
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-05-03 09:09:08 +0100

    initialized

---
+++ src/main/java/com/logikcode/springoauth2andsocial/SpringOauth2AndSocialsApplication.java
@@ -0,0 +1,13 @@
+package com.logikcode.springoauth2andsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2AndSocialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2AndSocialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logikcode/springoauth2andsocial/SpringOauth2AndSocialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logikcode.springoauth2andsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2AndSocialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
